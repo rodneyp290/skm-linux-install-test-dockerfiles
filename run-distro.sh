f9df9cca3ecb6f6b15c396daee2ba8e45343b6cf
Rodney Pilgrim <rodneyp290@gmail.com>
2019-05-23 22:20:36 +1000

    FIX: Add check for existing containers, and use `docker start` on existing containers to avoid name clash

--- run-distro.sh
+++ run-distro.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker run --name splashkit-${1} ${1}-splashkit
+if (docker ps -a | grep splashkit-${1} > /dev/null ); then
+  docker start splashkit-${1}
+else
+  docker run --name splashkit-${1} ${1}-splashkit
+fi
