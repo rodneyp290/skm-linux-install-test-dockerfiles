70660348bbb8440b51f040a825242d40aacbb7b4
Rodney Pilgrim <rodneyp290@gmail.com>
2019-05-25 13:30:30 +1000

    ENHANCE: Move skm linux install from docker build to script to run by user

--- build-distro.sh
+++ build-distro.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
 cp cpp/program.cpp ${1}
+cp container-scripts/install-n-test.sh ${1}
+
 docker build -t ${1}-splashkit ${1}
-rm ${1}/program.cpp
+
+rm ${1}/program.cpp ${1}/install-n-test.sh
